ba5e478ea44b2ae25f7db271d64cc464cce61b56
Mary Stewart <mary@stoo.org>
2015-05-17 00:46:23 -0400

    refine design and add form fields

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$(function(){
+
+	//Submit a Prompt functionality
+
+	//Listen for a click event on a field
+
+	//Have an object to hold text, date and user
+
+	//Post to the back-end and onto page divs
+
+	//GET new prompts from back-end
+
+	//Populate into prompt divs under correct genre
+
+
+
+
+
+
+
+	//Submit a writing to a prompt functionality
+
+
+	//Create a User functionality
+
+
+
+
+})
